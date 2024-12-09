8b6e768b8843d39e19dbe2bdf0cbc0860abcffe5
Joseph Iglecias <igleciasjoseph@gmail.com>
2024-12-09 13:44:32 -0500

    Initial Commit

---
+++ VinylPlayer/VinylPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VinylPlayerApp.swift
+//  VinylPlayer
+//
+//  Created by Joseph Iglecias on 12/9/24.
+//
+
+import SwiftUI
+
+@main
+struct VinylPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
